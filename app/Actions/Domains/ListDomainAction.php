1098dde9ea7b309e11409263f61523d6792518a5
bounceafr <bounce.afr@gmail.com>
2026-03-21 04:25:53 +0200

    feat: fix domains prices

--- app/Actions/Domains/ListDomainAction.php
+++ app/Actions/Domains/ListDomainAction.php
@@ -16,7 +16,7 @@ final class ListDomainAction
     {
         return Domain::query()
             ->with('tldPricing', 'owner')
-            ->select(['id', 'uuid', 'name', 'registered_at', 'auto_renew', 'expires_at', 'status', 'owner_id'])
+            ->select(['id', 'uuid', 'name', 'registered_at', 'auto_renew', 'expires_at', 'status', 'owner_id', 'tld_pricing_id', 'is_custom_price', 'custom_price', 'custom_price_currency'])
             ->latest()
             ->get();
     }
